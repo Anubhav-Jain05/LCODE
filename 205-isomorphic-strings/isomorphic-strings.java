1014b73bb1a321a2f8c73c3b6d9dfced3463bd0d
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-04-02 22:11:20 +0530

    Time: 25 ms (9.45%) | Memory: 42.6 MB (26.81%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,23 +1,37 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character,Character> map = new HashMap<>();
+        HashMap<Character,Character> smap=new HashMap<>();
+        HashMap<Character,Character> tmap=new HashMap<>();
         for(int i=0;i<s.length();i++){
             char s1=s.charAt(i);
             char t1=t.charAt(i);
-            if(map.containsKey(s1)){
-                if(map.get(s1).equals(t1)){
-                    continue;
-                }else{
-                    return false;
-                }
-            }else{
-                if(!map.containsValue(t1)){
-                    map.put(s1,t1);
-                }else{
-                    return false;
-                }
+            if(smap.containsKey(s1) && smap.get(s1) != t1 || tmap.containsKey(t1) && tmap.get(t1) != s1){
+                return false;
             }
+            
+            smap.put(s1,t1);
+            tmap.put(t1,s1);
         }
         return true;
+
+        // HashMap<Character,Character> map = new HashMap<>();
+        // for(int i=0;i<s.length();i++){
+        //     char s1=s.charAt(i);
+        //     char t1=t.charAt(i);
+        //     if(map.containsKey(s1)){
+        //         if(map.get(s1).equals(t1)){
+        //             continue;
+        //         }else{
+        //             return false;
+        //         }
+        //     }else{
+        //         if(!map.containsValue(t1)){
+        //             map.put(s1,t1);
+        //         }else{
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return true;
     }
 }
