746ca7a47b4648c2e5bcbbd7b487327290c10524
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-04-05 14:41:04 +0530

    Time: 1 ms (100.00%) | Memory: 41.6 MB (95.62%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String makeGood(String s) {
+        StringBuilder result= new StringBuilder();
+
+        for(char ch : s.toCharArray()){
+            if(result.length() > 0 && Math.abs(result.charAt(result.length()-1)-ch) == 32){
+                result.deleteCharAt(result.length()-1);
+            }
+            else{
+                result.append(ch);
+            }
+        }
+        return result.toString();
+    }
+}
