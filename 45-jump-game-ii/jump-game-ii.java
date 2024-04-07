77b87fd3e1591e1ad99d98192c26e0654a7a066f
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-04-07 12:32:38 +0530

    Time: 863 ms (5.05%) | Memory: 50.6 MB (5.09%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int jump(int[] nums) {
+        return minJump(nums,0,new HashMap<Integer,Integer>());
+    }
+    private int minJump(int[] nums,int currentIndex,HashMap<Integer,Integer>map){
+        if(currentIndex >= nums.length-1)return 0;
+        if(nums[currentIndex]==0)return 10001;
+        int currentKey=currentIndex;
+        if(map.containsKey(currentKey)){
+            return map.get(currentKey);
+        }
+        int ans=10001;
+        int currjump=nums[currentIndex];
+        for(int i=1;i<=currjump;i++){
+            int tempans= 1 + minJump(nums,currentIndex + i,map);
+            ans=Math.min(tempans,ans);
+        }
+        map.put(currentKey,ans);
+        return ans;
+    }
+}
