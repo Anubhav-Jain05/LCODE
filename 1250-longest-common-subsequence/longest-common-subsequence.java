a2ce6c5e5d0bd9f81bb62634f641b7474eb8aa3c
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-05-05 16:45:21 +0530

    Time: 19 ms (86.72%) | Memory: 50.9 MB (39.05%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,24 +1,40 @@
 class Solution {
-    public int longestCommonSubsequence(String s1, String s2) {
-        return LCS(s1,s2,0,0,s1.length(),s2.length(),new HashMap<String,Integer>());
-    }
-    private int LCS(String s1,String s2,int i ,int j,int n,int m, HashMap<String,Integer> map){
-        if(i >= n || j >= m)return 0;
-        int ans =0;
-        String currentKey= i +"-"+j; 
-        // TC depends on i and j where i is go to the length of n and j is go to the length of m
-        // so the TC will be O(n * m) and SC will be same 
-        if(map.containsKey(currentKey)){
-            return map.get(currentKey);
+    public int longestCommonSubsequence(String text1, String text2) {
+    //     return LCS(s1,s2,0,0,s1.length(),s2.length(),new HashMap<String,Integer>());
+    // }
+    // private int LCS(String s1,String s2,int i ,int j,int n,int m, HashMap<String,Integer> map){
+    //     if(i >= n || j >= m)return 0;
+    //     int ans =0;
+    //     String currentKey= i +"-"+j; 
+    //     // TC depends on i and j where i is go to the length of n and j is go to the length of m
+    //     // so the TC will be O(n * m) and SC will be same 
+    //     if(map.containsKey(currentKey)){
+    //         return map.get(currentKey);
+    //     }
+    //     if(s1.charAt(i) == s2.charAt(j)){
+    //         ans = 1 + LCS(s1,s2,i + 1,j + 1,n,m,map);
+    //     }else{
+    //         int consider_i=LCS(s1,s2,i,j+1,n,m,map);
+    //         int consider_j=LCS(s1,s2,i+1,j,n,m,map);
+    //         ans=Math.max(consider_i,consider_j);
+    //     }
+    //     map.put(currentKey,ans);
+    //     return ans;
+
+    int n=text1.length();
+    int m=text2.length();
+    int[][]dp = new int[n+1][m+1];
+    for(int i=1;i<n + 1;i++){
+        for(int j=1;j<m+1;j++){
+            if(text1.charAt(i-1) == text2.charAt(j-1)){
+                dp[i][j]=1+dp[i-1][j-1];
+            }
+            else{
+                dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+            }
         }
-        if(s1.charAt(i) == s2.charAt(j)){
-            ans = 1 + LCS(s1,s2,i + 1,j + 1,n,m,map);
-        }else{
-            int consider_i=LCS(s1,s2,i,j+1,n,m,map);
-            int consider_j=LCS(s1,s2,i+1,j,n,m,map);
-            ans=Math.max(consider_i,consider_j);
-        }
-        map.put(currentKey,ans);
-        return ans;
     }
+    return dp[n][m];
+    }
+
 }
