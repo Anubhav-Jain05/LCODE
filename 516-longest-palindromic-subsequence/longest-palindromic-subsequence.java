c2c049d5cce3e64f6696a2b9396d5f14d7872900
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-05-05 13:18:29 +0530

    Time: 756 ms (5.24%) | Memory: 148.3 MB (5.03%) - LeetSync

---
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        return LCS(s,new StringBuilder(s).reverse().toString(),0,0,s.length(),s.length(),new HashMap<String,Integer>());
+    }
+    private int LCS(String s1,String s2,int i ,int j,int n,int m, HashMap<String,Integer> map){
+        if(i >= n || j >= m)return 0;
+        int ans =0;
+        String currentKey= i +"-"+j; 
+        // TC depends on i and j where i is go to the length of n and j is go to the length of m
+        // so the TC will be O(n * m) and SC will be same 
+        if(map.containsKey(currentKey)){
+            return map.get(currentKey);
+        }
+        if(s1.charAt(i) == s2.charAt(j)){
+            ans = 1 + LCS(s1,s2,i + 1,j + 1,n,m,map);
+        }else{
+            int consider_i=LCS(s1,s2,i,j+1,n,m,map);
+            int consider_j=LCS(s1,s2,i+1,j,n,m,map);
+            ans=Math.max(consider_i,consider_j);
+        }
+        map.put(currentKey,ans);
+        return ans;
+    }
+    
+}
