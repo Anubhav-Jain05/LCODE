00ddf0d83f1930d39253814810bb6d29515ce0da
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-05-19 11:15:21 +0530

    Time: 2 ms (97.62%) | Memory: 45 MB (24.58%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,73 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> map =new HashMap<>();
-        int[] res= new int[2];
+        HashMap<Integer,Integer> map = new HashMap<>();
+
         for(int i=0;i<nums.length;i++){
-            if(map.containsKey(target-nums[i])){
-                res[0]=map.get(target-nums[i]);
-                res[1]=i;
+            int complement=target-nums[i];
+
+            if(map.containsKey(complement)){
+                return new int [] {map.get(complement),i};
+            }else{
+                map.put(nums[i],i);
             }
-            map.put(nums[i],i);
         }
-        return res;
-       
-    //    HashMap<Integer,Integer> map =new HashMap<>();
-    //    int[] res= new int[2];
-    //    for(int i=0;i<nums.length;i++){
-    //        if(map.containsKey(target-nums[i])){
-    //            res[0]=map.get(target-nums[i]);
-    //            res[1]=i;
-    //        }
-    //        map.put(nums[i],i);
-    //    }
-    //    return res;
-        // HashMap<Integer,Integer> hs= new HashMap<>();
-        // int[] res= new int[2];
-        // for(int i=0;i<nums.length;i++){
-        //     if(hs.containsKey(target-nums[i])){
-        //         res[0]=hs.get(target-nums[i]);
-        //         res[1]=i;
-        //     }
-        //     hs.put(nums[i],i);
-        // }
-        // return res;
-        // int[] res= new int[2];
-        // for(int i=0;i<nums.length-1;i++){
-        //     for(int j=i+1;j<nums.length;j++){
-        //         if(nums[i] + nums[j] == target){
-        //             res[0]=i;
-        //             res[1]=j;
-        //         }
-        //     }
-        // }
-        // return res;
-
-        
-        
-
-        
-
-    //    HashMap<Integer,Integer> map=new HashMap<>();
-    //    int[] res= new int[2];
-    //    for(int i=0;i<nums.length;i++){
-    //        if(map.containsKey(target-nums[i])){
-    //            res[0]=map.get(target-nums[i]);
-    //            res[1]=i;
-    //            return res;
-    //        }
-    //        map.put(nums[i],i);
-    //    }
-    //    return res;
-
-        // int [] res= new int[2];
-        // for(int i=0;i<nums.length-1;i++){
-        //     for(int j=i+1;j<nums.length;j++){
-        //         if(nums[i]+nums[j]==target){
-        //             res[0]=i;
-        //             res[1]=j;
-        //         }
-        //     }
-        // }
-        // return res;
+        return new int[2];
     }
 }
