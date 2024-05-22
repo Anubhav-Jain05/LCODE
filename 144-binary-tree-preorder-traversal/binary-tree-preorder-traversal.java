886b7066f0ae0fb68b77e4a86a3e23137f221bbc
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-05-22 22:23:15 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (84.98%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -15,18 +15,17 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        preOrder(root,ans);
-        return ans;
+       List<Integer> ans= new ArrayList<>();
+       preOrder(root,ans);
+       return ans;
     }
-    private void preOrder(TreeNode root,List<Integer>ans){
-        if(root== null)return;
-
-        ans.add(root.val); // first add the root node in ans 
-
-        preOrder(root.left,ans); // after add the root node in ans we want to traversal from left;
+    private void preOrder(TreeNode root,List<Integer> ans){
+        if(root == null){
+            return;
+        }
+        ans.add(root.val);
+        preOrder(root.left,ans);
         preOrder(root.right,ans);
-
         return;
     }
 }
