1fe28fb92389578125224c3dab84acfc20c5d868
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-05-22 22:27:28 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (86.36%) - LeetSync

--- 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.java
+++ 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.java
@@ -19,20 +19,17 @@ class Node {
 
 class Solution {
     public List<Integer> preorder(Node root) {
-        List<Integer> ans= new ArrayList<>();
-        nAry(root,ans);
+        List<Integer>ans= new ArrayList<>();
+        preOrder(root,ans);
         return ans;
     }
-    private void nAry(Node root, List<Integer> ans){
+    private void preOrder(Node root,List<Integer> ans){
         if(root == null)return;
 
         ans.add(root.val);
-
-        // List<Node>childern=root.children;
         for(Node child:root.children){
-            nAry(child,ans);
+            preOrder(child,ans);
         }
-
         return;
     }
 }
