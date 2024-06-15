cc286013f47bb0dd9b140d6a8d38e0c1b708cf46
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-06-15 14:11:05 +0530

    Time: 2 ms (71.04%) | Memory: 44.5 MB (22.71%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -13,12 +13,38 @@
  *     }
  * }
  */
+// class Solution {
+//     public TreeNode buildTree(int[] preorder, int[] inorder) {
+//         int[] preIndex={0};
+//         return constructBT(preorder,inorder,preIndex,0,inorder.length-1);
+//     }
+//     private TreeNode constructBT(int[] preorder,int[] inorder,int[] preIndex,int instart,int inend){
+//         if(preIndex[0] >= preorder.length || instart > inend){
+//             return null;
+//         }
+//         int currentVal=preorder[preIndex[0]];
+//         TreeNode currentNode=new TreeNode(currentVal);
+//         preIndex[0] += 1;
+
+//         int i=-1;
+//         for(i=instart;i<inend;i++){
+//             if(inorder[i]==currentVal){
+//                 break;
+//             }
+//         }
+//         currentNode.left=constructBT(preorder,inorder,preIndex,instart,i-1);
+//         currentNode.right=constructBT(preorder,inorder,preIndex,i+1,inend);
+//         return currentNode;
+//     }
+// }
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         int[] preIndex={0};
-        return constructBT(preorder,inorder,preIndex,0,inorder.length-1);
+        HashMap<Integer,Integer>inorderMap=buildMap(inorder);
+        return constructBT(preorder,inorder,preIndex,0,inorder.length-1,inorderMap);
     }
-    private TreeNode constructBT(int[] preorder,int[] inorder,int[] preIndex,int instart,int inend){
+    private TreeNode constructBT(int[] preorder,int[] inorder,int[] preIndex,int instart,int inend,
+     HashMap<Integer,Integer>inorderMap){
         if(preIndex[0] >= preorder.length || instart > inend){
             return null;
         }
@@ -26,14 +52,16 @@ class Solution {
         TreeNode currentNode=new TreeNode(currentVal);
         preIndex[0] += 1;
 
-        int i=-1;
-        for(i=instart;i<inend;i++){
-            if(inorder[i]==currentVal){
-                break;
-            }
-        }
-        currentNode.left=constructBT(preorder,inorder,preIndex,instart,i-1);
-        currentNode.right=constructBT(preorder,inorder,preIndex,i+1,inend);
+       int i=inorderMap.get(currentVal);
+        currentNode.left=constructBT(preorder,inorder,preIndex,instart,i-1,inorderMap);
+        currentNode.right=constructBT(preorder,inorder,preIndex,i+1,inend,inorderMap);
         return currentNode;
+    }
+    private HashMap<Integer,Integer>buildMap(int[] inorder){
+        HashMap<Integer,Integer> inorderMap=new HashMap<>();
+        for(int i=0;i<inorder.length;i++){
+            inorderMap.put(inorder[i], i);
+        }
+        return inorderMap;
     }
 }
