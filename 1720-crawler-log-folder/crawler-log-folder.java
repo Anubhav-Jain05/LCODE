54fa974236d067b41ec3d3d482d0cad662bd42b8
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-07-11 22:21:58 +0530

    Time: 2 ms (44.41%) | Memory: 41.8 MB (68.18%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.java
+++ 1720-crawler-log-folder/crawler-log-folder.java
@@ -1,15 +1,26 @@
 class Solution {
     public int minOperations(String[] logs) {
-        Stack<String> path=new Stack<>();
-        for(String log : logs){
-            if(log.equals("../")){
+        Stack<String> path= new Stack<>();
+        for(String str : logs){
+            if(str.equals("../")){
                 if(!path.isEmpty()){
                     path.pop();
                 }
-            }else if(!log.equals("./")){
-                path.push(log);
+            }else if(!str.equals("./")){
+                path.push(str);
             }
         }
         return path.size();
+        // Stack<String> path=new Stack<>();
+        // for(String log : logs){
+        //     if(log.equals("../")){
+        //         if(!path.isEmpty()){
+        //             path.pop();
+        //         }
+        //     }else if(!log.equals("./")){
+        //         path.push(log);
+        //     }
+        // }
+        // return path.size();
     }
 }
