16d018e68b53fe0803ef410e71ba3e320903949b
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-07-20 23:21:29 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (18.33%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,17 +1,17 @@
 class Solution {
     public int rob(int[] nums) {
-        HashMap<Integer,Integer> map =new HashMap<>();
-        return maximumAmt(nums,0,map);
+        HashMap<Integer,Integer> map = new HashMap<>();
+        return maxProfit(nums,0,map);
     }
-    private int maximumAmt(int[] nums,int currentIndex,HashMap<Integer,Integer> map){
+    private int maxProfit(int[] nums,int currentIndex,HashMap<Integer,Integer> map){
         if(currentIndex >= nums.length)return 0;
-        int currentKey= currentIndex;
+        int currentKey=currentIndex;
         if(map.containsKey(currentKey)){
             return map.get(currentKey);
         }
-        int rob=nums[currentIndex] + maximumAmt(nums,currentIndex + 2,map);
-        int not_rob=maximumAmt(nums,currentIndex + 1,map);
-        map.put(currentKey,Math.max(rob,not_rob));
+        int rob=nums[currentIndex] + maxProfit(nums,currentIndex + 2,map);
+        int no_rob=maxProfit(nums,currentIndex + 1,map);
+        map.put(currentKey,Math.max(rob,no_rob));
         return map.get(currentKey);
 
     }
