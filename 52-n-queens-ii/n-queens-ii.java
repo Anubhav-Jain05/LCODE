95d6b716e5e80e3039f5a41343d3c50b8588b13e
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-09-15 00:44:52 +0530

    Time: 12 ms (7.23%) | Memory: 42.6 MB (8.40%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.java
@@ -0,0 +1,85 @@
+class Solution {
+    public int totalNQueens(int n) {
+         List<List<String>> answer=new ArrayList<>();
+        char[][]board=new char[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]='.';
+            }
+        }
+        nQueens(board,0,n,answer);
+        return answer.size();
+    }
+    private void nQueens(char[][] board,int currentRow,int n,List<List<String>> answer){
+        if(currentRow == n){
+            answer.add(constructAns(board,n));
+            return;
+        }
+         for(int currentCol=0;currentCol<n;currentCol++){
+            if(isValid(board,currentRow,currentCol,n)){
+                board[currentRow][currentCol]='Q';
+                nQueens(board,currentRow + 1,n,answer);
+                board[currentRow][currentCol]='.';
+            }
+         }
+         return;
+    }
+    private List<String> constructAns(char[][] board,int n){
+        List<String> current=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            String currentRow="";
+            for(int j=0;j<n;j++){
+                currentRow+=board[i][j];
+            }
+            current.add(currentRow);
+
+        }
+        return current;
+    }
+    private boolean isValid(char[][] board,int currentRow,int currentCol,int n){
+        return isRowValid(board,currentRow,n) &&
+                isColValid(board,currentCol,n) &&
+                isdiagonal(board,currentRow,currentCol,n);
+    }
+    private boolean isRowValid(char[][] board,int currentRow,int n){
+        for(int currentCol=0;currentCol<n;currentCol ++){
+            if(board[currentRow][currentCol] == 'Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    private boolean isColValid(char[][] board,int currentCol,int n){
+         for(int currentRow=0;currentRow<n;currentRow ++){
+            if(board[currentRow][currentCol] == 'Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    private boolean isdiagonal(char[][] board,int currentRow,int currentCol,int n){
+        //leftUpDiagonal
+        int x=currentRow;
+        int y=currentCol;
+
+        while(x >=0 && y >= 0){
+            if(board[x][y]=='Q'){
+                return false;
+            }
+            x-=1;
+            y-=1;
+        }
+        //rightupDiagonal
+        x=currentRow;
+        y=currentCol;
+        while(x >=0 && y < n){
+            if(board[x][y] == 'Q'){
+                return false;
+            }
+            x-=1;
+            y+=1;
+        }
+        return true;
+    }
+    
+}
