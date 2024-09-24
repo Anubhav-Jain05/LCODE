d7bfdd4bc12f3c42426bf722d21385885fb8be62
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-09-25 01:07:00 +0530

    Time: 3 ms (7.16%) | Memory: 44.3 MB (5.68%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,19 +1,20 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        HashMap<Integer,Integer>map = new HashMap<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
         return Math.min(minCost(cost,0,map),minCost(cost,1,map));
     }
-    private int minCost(int[] cost,int currentIndex,HashMap<Integer,Integer>map){
+    private int minCost(int[] cost,int currentIndex,HashMap<Integer,Integer> map){
         if(currentIndex == cost.length)return 0;
         if(currentIndex > cost.length)return 1001;
+
         int currentKey=currentIndex;
         if(map.containsKey(currentKey)){
             return map.get(currentKey);
         }
-        int one_jump=cost[currentIndex] + minCost(cost,currentIndex + 1,map);
-        int two_jump=cost[currentIndex] + minCost(cost,currentIndex + 2,map);
-        map.put(currentKey,Math.min(one_jump,two_jump));
 
+        int oneJump=cost[currentIndex] + minCost(cost,currentIndex + 1,map);
+        int twoJump=cost[currentIndex] + minCost(cost,currentIndex + 2,map);
+        map.put(currentKey,Math.min(oneJump,twoJump));
         return map.get(currentKey);
     }
 }
