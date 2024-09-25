cc2202aeaadc2728435a538029254f0ab976d868
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-09-26 00:09:09 +0530

    Time: 634 ms (5.01%) | Memory: 192.3 MB (5.05%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,56 +1,30 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-    //     int totalSum=0;
-    //     for(int num:nums){
-    //         totalSum += num;
-    //     }
-    //     if(totalSum % 2 != 0)return false;
-    //     return isPossible(nums,0,totalSum / 2,new HashMap<String,Boolean>());
-    // }
-    // private boolean isPossible(int[] nums,int currentIndex,int targetSum,HashMap<String,Boolean>map){
-    //     if(targetSum == 0)return true;
-    //     if(currentIndex >= nums.length)return false;
-    //     String currentKey=Integer.toString(currentIndex) +"_"+Integer.toString(targetSum);
-    //     if(map.containsKey(currentKey)){
-    //         return map.get(currentKey);
-    //     }
-    //     boolean consider=false;
-    //     if(nums[currentIndex] <= targetSum){
-    //          consider=isPossible(nums,currentIndex+1,targetSum-nums[currentIndex],map);
-    //     }
-    //     if(consider){
-    //         map.put(currentKey,true);
-    //         return true;
-    //     }
-    //     boolean notConsider=isPossible(nums,currentIndex + 1,targetSum,map);
-    //     map.put(currentKey,consider || notConsider);
-    //     return map.get(currentKey);
-    HashMap<String,Boolean>map = new HashMap<>();
-    int totalSum=0;
-    for(int num:nums){
-        totalSum += num;
+        HashMap<String,Boolean> map = new HashMap<>();
+        int total_sum =0;
+        for(int num : nums){
+            total_sum += num;
+        }
+        if(total_sum % 2 != 0 )return false;
+        return isPossible(nums,0,total_sum / 2,map);
     }
-    if(totalSum % 2!= 0)return false;
-    return isPossible(nums,0,totalSum / 2,map);
-
-    }
-    private boolean isPossible(int[] nums,int currentItem,int targetSum,HashMap<String,Boolean>map){
+    private boolean isPossible(int[] nums,int currentIndex,int targetSum,HashMap<String,Boolean> map){
         if(targetSum == 0)return true;
-        if(currentItem >= nums.length)return false;
-        String currentKey=Integer.toString(currentItem) + "-"+Integer.toString(targetSum);
+        if(currentIndex >= nums.length)return false;
+
+        String currentKey= Integer.toString(currentIndex) + "-" + Integer.toString(targetSum);
         if(map.containsKey(currentKey)){
             return map.get(currentKey);
         }
-
-        boolean consider=false;
-        if(nums[currentItem] <= targetSum){
-            consider=isPossible(nums,currentItem + 1,targetSum-nums[currentItem],map);
+        boolean consider = false;
+        if(nums[currentIndex] <= targetSum){
+            consider = isPossible(nums,currentIndex + 1,targetSum - nums[currentIndex],map);
         }
         if(consider){
             map.put(currentKey,true);
             return true;
         }
-        boolean not_consider=isPossible(nums,currentItem + 1,targetSum,map);
+        boolean not_consider= isPossible(nums,currentIndex + 1,targetSum,map);
         map.put(currentKey,consider || not_consider);
         return map.get(currentKey);
     }
