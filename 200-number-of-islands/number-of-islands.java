cfb6a6a18f1c94709759261a8daa754e9539c85b
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-10-07 23:30:51 +0530

    Time: 2 ms (99.76%) | Memory: 49.6 MB (38.89%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,31 +1,30 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        if(grid == null)return 0;
-        int row=grid.length;
-        int col=grid[0].length;
-        int count =0;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(grid[i][j] == '1'){
-                    count ++;
-                    dfs(grid,i,j);
+        int ans=0;
+        int m=grid.length;
+        int n=grid[0].length;
+
+        for(int currentrow=0;currentrow < m;currentrow++){
+            for(int currentCol=0;currentCol<n;currentCol++){
+                if(grid[currentrow][currentCol] == '1'){
+                    sinkIsland(grid,currentrow,currentCol,m,n);
+                    ans += 1;
                 }
             }
         }
-        return count;
+        return ans;
     }
-    private void dfs(char[][] grid,int row,int col){
-        int numrow=grid.length;
-        int numcol=grid[0].length;
+    private void sinkIsland(char[][] grid,int currentrow,int currentCol,int m,int n){
+        if(currentrow < 0 || currentrow >= m || 
+            currentCol < 0 || currentCol >= n || 
+            grid[currentrow][currentCol] == '0')return;
 
-        if(row < 0 || col < 0 || row >= numrow || col >= numcol || grid[row][col]=='0'){
-            return;
-        }
-        grid[row][col]='0';
+        grid[currentrow][currentCol] = '0';
+        sinkIsland(grid,currentrow - 1 ,currentCol,m,n);
+        sinkIsland(grid,currentrow + 1,currentCol,m,n);
+        sinkIsland(grid,currentrow ,currentCol - 1,m,n);
+        sinkIsland(grid,currentrow ,currentCol + 1,m,n);
 
-        dfs(grid,row + 1,col);
-        dfs(grid,row-1,col);
-        dfs(grid,row,col + 1);
-        dfs(grid,row,col-1);
+
     }
 }
