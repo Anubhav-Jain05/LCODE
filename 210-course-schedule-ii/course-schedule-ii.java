c854ac4172d0cd9297a6679354aa7ef10baec690
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-10-12 15:50:14 +0530

    Time: 3 ms (98.81%) | Memory: 46 MB (39.31%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,61 +1,120 @@
+// class Solution {
+//     public int[] findOrder(int numCourses, int[][] prerequisites) {
+//         ArrayList<ArrayList<Integer>> graph = constructGraph(numCourses,prerequisites);
+//         boolean[] visited = new boolean[numCourses];
+//         ArrayList<Integer> ans=new ArrayList<>();
+//         for(int currentCourse=0;currentCourse<numCourses;currentCourse++){
+//             if(visited[currentCourse] == false){
+//                 if(hasOrder(graph,currentCourse,visited,new HashSet<Integer>(),ans)){
+//                     return new int[0];
+//                 }
+//             }
+//         }
+//         int[] answer= new int[numCourses];
+//         for(int i=0;i<numCourses;i++){
+//             answer[i]=ans.get(i);
+//         }
+//         return answer;
+//     }
+//     private boolean hasOrder(ArrayList<ArrayList<Integer>> graph,int currentCourse,boolean[] visited,
+//     HashSet<Integer>recStack,ArrayList<Integer> ans){
+//         visited[currentCourse]= true;
+
+//         recStack.add(currentCourse);
+
+//         for(int currentNeigh:graph.get(currentCourse)){
+//             if(visited[currentNeigh] == false){
+//                 if(hasOrder(graph,currentNeigh,visited,recStack,ans)){
+//                     return true;
+//                 }
+//                 else if(recStack.contains(currentNeigh)){
+//                     return true;
+//                 }
+//             }
+//         }
+//         recStack.remove(currentCourse);
+//         ans.add(currentCourse);
+//         return false;
+//     }
+//     private  ArrayList<ArrayList<Integer>> constructGraph(int n,int [][] edges){
+//          ArrayList<ArrayList<Integer>> graph=new ArrayList<>();
+
+//          for(int i=0;i<n;i++){
+//             graph.add(new ArrayList<Integer>());
+//          }
+
+//          for(int[] currentEdge:edges){
+//             int a=currentEdge[0];
+//             int b=currentEdge[1];
+//             graph.get(a).add(b);
+//          }
+//          return graph;
+//     }
+// }
+
+import java.util.*;
+
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        ArrayList<ArrayList<Integer>> graph = constructGraph(numCourses,prerequisites);
+        ArrayList<ArrayList<Integer>> graph = constructGraph(numCourses, prerequisites);
         boolean[] visited = new boolean[numCourses];
-        int[] indegree=new int[numCourses];
+        boolean[] recStack = new boolean[numCourses];  // Used for cycle detection
+        ArrayList<Integer> ans = new ArrayList<>();
 
-        for(int currentCourse=0;currentCourse < numCourses;currentCourse ++){
-            for(int currentNeigh:graph.get(currentCourse)){
-                indegree[currentNeigh] += 1;
-            }
-        }
-        Queue<Integer> queue= new LinkedList<>();
-        ArrayList<Integer>ans= new ArrayList<>();
-
-        for(int i=0;i<numCourses;i++){
-            if(indegree[i] == 0){
-                queue.add(i);
-            }
-        }
-
-        while(!queue.isEmpty()){
-            int currentNode = queue.remove();
-
-            if(visited[currentNode]){
-                continue;
-            }
-            visited[currentNode] = true;
-            ans.add(currentNode);
-
-            for(int neigh:graph.get(currentNode)){
-                indegree[neigh] -= 1;
-
-                if(indegree[neigh] == 0){
-                    queue.add(neigh);
+        for (int currentCourse = 0; currentCourse < numCourses; currentCourse++) {
+            if (!visited[currentCourse]) {
+                if (hasOrder(graph, currentCourse, visited, recStack, ans)) {
+                    return new int[0];  // Return empty array if a cycle is detected
                 }
             }
         }
-        if(ans.size() != numCourses){
-            return new int[0];
+
+        // Reverse the result list to get the correct topological order
+        Collections.reverse(ans);
+
+        // Convert the ArrayList to an array
+        int[] answer = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            answer[i] = ans.get(i);
         }
-        int[] answer= new int[numCourses];
-        for(int i=0;i<numCourses;i++){
-            answer[i]=ans.get(i);
-        }
+
         return answer;
     }
-    private  ArrayList<ArrayList<Integer>> constructGraph(int n,int [][] edges){
-         ArrayList<ArrayList<Integer>> graph=new ArrayList<>();
 
-         for(int i=0;i<n;i++){
+    private boolean hasOrder(ArrayList<ArrayList<Integer>> graph, int currentCourse, boolean[] visited,
+                             boolean[] recStack, ArrayList<Integer> ans) {
+        visited[currentCourse] = true;
+        recStack[currentCourse] = true;  // Add to recursion stack
+
+        for (int currentNeigh : graph.get(currentCourse)) {
+            if (!visited[currentNeigh]) {
+                if (hasOrder(graph, currentNeigh, visited, recStack, ans)) {
+                    return true;  // Cycle detected
+                }
+            } else if (recStack[currentNeigh]) {
+                return true;  // Cycle detected
+            }
+        }
+
+        recStack[currentCourse] = false;  // Remove from recursion stack
+        ans.add(currentCourse);  // Post-order add to result (reverse needed later)
+        return false;
+    }
+
+    private ArrayList<ArrayList<Integer>> constructGraph(int n, int[][] edges) {
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<Integer>());
-         }
+        }
 
-         for(int[] currentEdge:edges){
-            int a=currentEdge[0];
-            int b=currentEdge[1];
-            graph.get(b).add(a);
-         }
-         return graph;
+        // Reverse the direction: if B → A, A depends on B, so graph[B].add(A)
+        for (int[] currentEdge : edges) {
+            int a = currentEdge[0];
+            int b = currentEdge[1];
+            graph.get(b).add(a);  // Add edge from b to a
+        }
+
+        return graph;
     }
 }
