1ac5d5d1776cdae865ede47f0ef52fa512a1e0c2
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-10-15 01:12:47 +0530

    Time: 3 ms (72.06%) | Memory: 53.7 MB (52.07%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,19 +1,35 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        int[] inDegree= new int[n+1];
-        int[] outDegree= new int[n+1];
+        // int[] inDegree= new int[n+1];
+        // int[] outDegree= new int[n+1];
 
+        // for(int[] currentTrust:trust){
+        //     int a=currentTrust[0];
+        //     int b=currentTrust[1];
+
+        //     inDegree[b] += 1;
+        //     outDegree[a] += 1;
+
+        // }
+        // for(int currentPeople=1;currentPeople <= n;currentPeople ++){
+        //     if(inDegree[currentPeople] == n-1 && outDegree[currentPeople] == 0){
+        //         return currentPeople;
+        //     }
+        // }
+        // return -1;
+
+        int[] indegree= new int[n+1];
+        int[] outdegree=new int[n+1];
         for(int[] currentTrust:trust){
             int a=currentTrust[0];
             int b=currentTrust[1];
 
-            inDegree[b] += 1;
-            outDegree[a] += 1;
-
+            indegree[b] += 1;
+            outdegree[a] += 1;
         }
-        for(int currentPeople=1;currentPeople <= n;currentPeople ++){
-            if(inDegree[currentPeople] == n-1 && outDegree[currentPeople] == 0){
-                return currentPeople;
+        for(int currentpeople=1;currentpeople <= n;currentpeople++){
+            if(indegree[currentpeople] == n-1 && outdegree[currentpeople] == 0){
+                return currentpeople;
             }
         }
         return -1;
