fcbc8e60f1c83e1edc03c8bb697e2fba439660f2
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-11-10 18:30:15 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (27.89%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,22 +1,37 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        // int n= numRows.length;
-        List<List<Integer>> list = new ArrayList<>();
-        for(int row=1;row<=numRows;row++){
-            list.add(generaterow(row));
-        }
-        return list;
+    //     List<List<Integer>> list = new ArrayList<>();
+    //     for(int row=1;row<=numRows;row++){
+    //         list.add(generaterow(row));
+    //     }
+    //     return list;
+    // }
+    // private static List<Integer> generaterow(int row){
+    //     long ans=1;
+    //     List<Integer>res=new ArrayList<>();
+    //     res.add(1);
+    //     for(int col=1;col<row;col++){
+    //         ans=ans * (row-col);
+    //         ans=ans/col;
+    //         res.add((int)ans);
+    //     }
+    //     return res;
+
+    List<List<Integer>> list = new ArrayList<>();
+    for(int row=1;row<=numRows;row++){
+        list.add(generaterow(row));
+    }
+    return list;
     }
     private static List<Integer> generaterow(int row){
         long ans=1;
-        List<Integer>res=new ArrayList<>();
-        res.add(1);
+        List<Integer> list= new ArrayList<>();
+        list.add(1);
         for(int col=1;col<row;col++){
             ans=ans * (row-col);
             ans=ans/col;
-            res.add((int)ans);
+            list.add((int)ans);
         }
-        return res;
-    
+        return list;
     }
 }
