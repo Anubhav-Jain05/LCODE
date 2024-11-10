35a9f1a7c63cc0868a19bc018576961c51ccdbd9
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-11-10 18:38:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (11.20%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,13 +1,28 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int count=0,count1=0,count2=0;
+        int count =0,count1=0,count2=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]==0)count++;
             else if(nums[i]==1)count1++;
             else count2++;
         }
-        for(int i=0;i<count;i++)nums[i]=0;
-        for(int i=count;i<count+count1;i++)nums[i]=1;
-        for(int i=count+count1;i<nums.length;i++)nums[i]=2;
+        for(int i=0;i<count;i++){
+            nums[i]=0;
+        }
+        for(int i=count;i<count+count1;i++){
+            nums[i]=1;
+        }
+        for(int i=count + count1;i<nums.length;i++){
+            nums[i]=2;
+        }
+        // int count=0,count1=0,count2=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]==0)count++;
+        //     else if(nums[i]==1)count1++;
+        //     else count2++;
+        // }
+        // for(int i=0;i<count;i++)nums[i]=0;
+        // for(int i=count;i<count+count1;i++)nums[i]=1;
+        // for(int i=count+count1;i<nums.length;i++)nums[i]=2;
     }
 }
