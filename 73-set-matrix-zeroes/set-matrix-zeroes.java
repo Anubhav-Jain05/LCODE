905222c5ca9ef40f4049f5872717500f73101658
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-11-11 23:48:21 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (19.14%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,11 +1,11 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int n= matrix.length;
+        int n=matrix.length;
         int m=matrix[0].length;
 
         boolean col_0=false;
         for(int i=0;i<n;i++){
-            if(matrix[i][0]==0)col_0=true;
+            if(matrix[i][0] == 0)col_0=true;
             for(int j=1;j<m;j++){
                 if(matrix[i][j]==0){
                     matrix[i][0]=0;
@@ -13,6 +13,7 @@ class Solution {
                 }
             }
         }
+
         for(int i=1;i<n;i++){
             for(int j=1;j<m;j++){
                 if(matrix[i][0]==0 || matrix[0][j]==0){
@@ -20,7 +21,8 @@ class Solution {
                 }
             }
         }
-        if(matrix[0][0] ==0){
+
+        if(matrix[0][0]==0){
             for(int j=1;j<m;j++){
                 matrix[0][j]=0;
             }
