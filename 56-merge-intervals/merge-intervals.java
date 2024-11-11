681b05671045d185d28fce0fc8936d11b2d6e278
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-11-12 01:32:55 +0530

    Time: 8 ms (88.64%) | Memory: 46.8 MB (11.57%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,26 +1,47 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if(intervals.length <=1)return intervals;
-
+        if(intervals.length <= 1)return intervals;
         Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
-
         List<int[]> list = new ArrayList<>();
         int[] currentIntervals=intervals[0];
         list.add(currentIntervals);
         for(int[] interval:intervals){
             int currentEnd=currentIntervals[1];
-            int nextStart=interval[0];
+            int nextstart=interval[0];
             int nextEnd=interval[1];
 
-            if(currentEnd >= nextStart){
-                /// if overlap
+            if(currentEnd >= nextstart){
                 currentIntervals[1]=Math.max(currentEnd,nextEnd);
-            }else{
-                //no overlap
-                currentIntervals=interval;
+            }
+            else{
+                currentIntervals = interval;
                 list.add(currentIntervals);
+
             }
         }
         return list.toArray(new int[list.size()][]);
+
+        // if(intervals.length <=1)return intervals;
+
+        // Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+
+        // List<int[]> list = new ArrayList<>();
+        // int[] currentIntervals=intervals[0];
+        // list.add(currentIntervals);
+        // for(int[] interval:intervals){
+        //     int currentEnd=currentIntervals[1];
+        //     int nextStart=interval[0];
+        //     int nextEnd=interval[1];
+
+        //     if(currentEnd >= nextStart){
+        //         /// if overlap
+        //         currentIntervals[1]=Math.max(currentEnd,nextEnd);
+        //     }else{
+        //         //no overlap
+        //         currentIntervals=interval;
+        //         list.add(currentIntervals);
+        //     }
+        // }
+        // return list.toArray(new int[list.size()][]);
     }
 }
