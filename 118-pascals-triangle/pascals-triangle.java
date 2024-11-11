1141e600d117f75cbb99461fd76d846eacbf674e
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-11-12 00:14:10 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (44.27%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,37 +1,37 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-    //     List<List<Integer>> list = new ArrayList<>();
-    //     for(int row=1;row<=numRows;row++){
-    //         list.add(generaterow(row));
-    //     }
-    //     return list;
-    // }
-    // private static List<Integer> generaterow(int row){
-    //     long ans=1;
-    //     List<Integer>res=new ArrayList<>();
-    //     res.add(1);
-    //     for(int col=1;col<row;col++){
-    //         ans=ans * (row-col);
-    //         ans=ans/col;
-    //         res.add((int)ans);
-    //     }
-    //     return res;
-
-    List<List<Integer>> list = new ArrayList<>();
-    for(int row=1;row<=numRows;row++){
-        list.add(generaterow(row));
-    }
-    return list;
-    }
-    private static List<Integer> generaterow(int row){
-        long ans=1;
-        List<Integer> list= new ArrayList<>();
-        list.add(1);
-        for(int col=1;col<row;col++){
-            ans=ans * (row-col);
-            ans=ans/col;
-            list.add((int)ans);
+        List<List<Integer>> list= new ArrayList<>();
+        for(int row=1;row<=numRows;row++){
+            list.add(generateRow(row));
         }
         return list;
     }
+    private List<Integer> generateRow(int row){
+        long ans=1;
+        List<Integer> list= new ArrayList<>();
+        list.add(1);
+        for(int col =1;col< row;col++){
+            ans=ans *(row-col);
+            ans=ans/col;
+            list.add((int) ans);
+        }
+        return list;
+    }
+    // List<List<Integer>> list = new ArrayList<>();
+    // for(int row=1;row<=numRows;row++){
+    //     list.add(generaterow(row));
+    // }
+    // return list;
+    // }
+    // private static List<Integer> generaterow(int row){
+    //     long ans=1;
+    //     List<Integer> list= new ArrayList<>();
+    //     list.add(1);
+    //     for(int col=1;col<row;col++){
+    //         ans=ans * (row-col);
+    //         ans=ans/col;
+    //         list.add((int)ans);
+    //     }
+    //     return list;
+    // }
 }
