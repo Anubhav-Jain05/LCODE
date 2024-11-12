401fd2a37af92c2fa23f82ed61a18150be88e6ee
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-11-13 00:30:53 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (49.50%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,14 +1,48 @@
 class Solution {
     public void sortColors(int[] nums) {
+        // int count=0,count1=0,count2=0;
+
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]==0){
+        //         count++;
+        //     }else if(nums[i]==1)count1++;
+        //     else count2++;
+        // }
+        // for(int i=0;i<count;i++){
+        //     nums[i]=0;
+        // }
+        // for(int i=count;i<count + count1;i++){
+        //     nums[i]=1;
+        // }
+        // for(int i=count + count1;i<nums.length;i++){
+        //     nums[i]=2;
+        // }
         int count=0,count1=0,count2=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==0)count++;
-            if(nums[i]==1)count1++;
-            if(nums[i]==2)count2++;
+        int n=nums.length-1;
+        int left=0;
+        int right=nums.length-1;
+
+        while(left <= right){
+            if(nums[left] == 0){
+                count++;
+                left++;
+            }else if(nums[left] == 1){
+                count1++;
+                left++;
+            }else if(nums[left] == 2){
+                count2 ++;
+                left++;
+            }
         }
-        for(int i=0;i<count;i++)nums[i]=0;
-        for(int i=count;i<count + count1;i++)nums[i]=1;
-        for(int i=count + count1;i<nums.length;i++)nums[i]=2;
+        for(int i=0;i<count;i++){
+            nums[i]=0;
+        }
+        for(int i=count;i<count+count1;i++){
+            nums[i]=1;
+        }
+        for(int i=count + count1;i<nums.length;i++){
+            nums[i]=2;
+        }
 
     }
 }
