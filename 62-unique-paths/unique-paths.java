6a6b189057f07551d7153465340151296c0aab9b
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-11-13 01:20:13 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (50.69%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,18 +1,11 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        return maxPath(m,n,0,0,new HashMap<String,Integer>());
-    }
-    private int maxPath(int m,int n,int row,int col,HashMap<String,Integer>map){
-        if(row == m-1 && col == n-1)return 1;
-        if(row >= m || col >= n )return 0;
-        String currentKey=Integer.toString(row)+"-"+Integer.toString(col);
-        if(map.containsKey(currentKey)){
-            return map.get(currentKey);
+        int N=n+m-2;
+        int r=m-1;
+        double res=1;
+        for(int i=1;i<=r;i++){
+            res=res * (N - r + i) / i;
         }
-        int rightways=maxPath(m,n,row,col + 1,map);
-        int downways=maxPath(m,n,row + 1,col,map);
-
-        map.put(currentKey,(rightways + downways));
-        return map.get(currentKey);
+        return (int)res;
     }
 }
