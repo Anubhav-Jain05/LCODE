b0c400ccf403b815de3c30a38dfae6d04e52988f
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-11-15 00:17:41 +0530

    Time: 2 ms (82.84%) | Memory: 43.2 MB (48.15%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
@@ -2,50 +2,21 @@ class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
         List<Integer> list= new ArrayList<>();
         Arrays.sort(nums);
-        int firstPosition=findfirstPosition(nums,target);
-        if(firstPosition == -1)return list;
-        int lastPosition=findlastPosition(nums,target);
-        for(int i=firstPosition;i<=lastPosition;i++){
-            list.add(i);
+        int first=-1;
+        int end=-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                list.add(i);
+                // if(first == -1){
+                //     first=i;
+                //     list.add(first);
+                // }
+                // else if(end == -1){
+                //     end=i;
+                // list.add(end);
+                // }   
+            }
         }
         return list;
-    }
-    private int findfirstPosition(int[] nums,int target){
-        int ans=-1;
-        int start=0;
-        int end=nums.length-1;
-        while(start <= end){
-            int mid=start + (end - start) / 2;
-            if(nums[mid] == target){
-                ans=mid;
-                end=mid-1;
-            }
-            else if(nums[mid] > target){
-                end=mid-1;
-            }
-            else{
-                start=mid + 1;
-            }
-        }
-        return ans;
-    }
-    private int findlastPosition(int[] nums,int target){
-        int ans=-1;
-        int start=0;
-        int end=nums.length-1;
-        while(start <= end){
-            int mid=start + (end - start) / 2;
-            if(nums[mid] == target){
-                ans=mid;
-                start=mid+1;
-            }
-            else if(nums[mid] > target){
-                end=mid-1;
-            }
-            else{
-                start=mid + 1;
-            }
-        }
-        return ans;
     }
 }
