6052b63b68c8fb85c3abfd70ca58ae587c27ece7
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-11-16 15:56:32 +0530

    Time: 25 ms (73.10%) | Memory: 40.7 MB (6.46%) - LeetSync

--- 278-first-bad-version/first-bad-version.java
+++ 278-first-bad-version/first-bad-version.java
@@ -3,18 +3,18 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int badversion=0;
-        int start=0;
+        int ans=-1;
+        int start= 1;
         int end=n;
         while(start <= end){
-            int mid = start + (end - start) / 2;
+            int mid=start + (end -start)/2;
             if(isBadVersion(mid)){
-                badversion=mid;
-                end= mid-1;
+                ans=mid;
+                end=mid-1;
             }else{
-                start = mid + 1;
+                start = mid+ 1;
             }
         }
-        return badversion;
+        return ans;
     }
 }
