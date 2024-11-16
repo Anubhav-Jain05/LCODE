00fe40264f08ce3b307e360e6a8adfb873b59b11
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-11-16 15:17:37 +0530

    Time: 3 ms (41.67%) | Memory: 43.2 MB (48.02%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
@@ -1,12 +1,48 @@
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
-        List<Integer> list= new ArrayList<>();
+        List<Integer> list =  new ArrayList<>();
         Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                list.add(i);
-            }
+        int firstPostion=findfirstIndex(nums,target);
+        if(firstPostion == -1)return list;
+        int endPosition=findendIndex(nums,target);
+        for(int i=firstPostion;i<=endPosition;i++){
+            list.add(i);
         }
         return list;
     }
+    private int findfirstIndex(int[] nums,int target){
+        int start = 0;
+        int end = nums.length -1;
+        int first= -1;
+        while(start <= end){
+            int mid=start + (end-start) / 2;
+            if(nums[mid]  < target){
+                start= mid + 1;
+            }else if(nums[mid] > target){
+                end = mid -1;
+            }else{
+                first= mid;
+                end  = mid -1;
+            }
+        }
+        return first;
+    }
+    private int findendIndex(int[] nums,int target){
+        int last = -1;
+        int start=0;
+        int end = nums.length-1;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] < target){
+                start = mid +1 ;
+            }else if(nums[mid] > target){
+                end= mid -1;
+            }else {
+                last= mid;
+                start= mid + 1;
+            }
+        }
+        return last;
+    }
+
 }
