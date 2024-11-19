90b9d0b7208a90ad801b2d48b847bf44506ec77c
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-11-20 00:01:57 +0530

    Time: 602 ms (5.09%) | Memory: 192.4 MB (5.04%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,31 +1,31 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         HashMap<String,Boolean> map = new HashMap<>();
-        int total_sum =0;
-        for(int num : nums){
-            total_sum += num;
+        int targetSum=0;
+        for(int num:nums){
+            targetSum+=num;
         }
-        if(total_sum % 2 != 0 )return false;
-        return isPossible(nums,0,total_sum / 2,map);
+        if(targetSum % 2 != 0)return false;
+        return isPartition(nums,0,targetSum / 2,map);
     }
-    private boolean isPossible(int[] nums,int currentIndex,int targetSum,HashMap<String,Boolean> map){
-        if(targetSum == 0)return true;
+    private boolean isPartition(int[] nums,int currentIndex,int target,HashMap<String,Boolean>map){
+        if(target == 0)return true;
         if(currentIndex >= nums.length)return false;
 
-        String currentKey= Integer.toString(currentIndex) + "-" + Integer.toString(targetSum);
+        String currentKey=Integer.toString(currentIndex) +"-"+Integer.toString(target);
         if(map.containsKey(currentKey)){
             return map.get(currentKey);
         }
         boolean consider = false;
-        if(nums[currentIndex] <= targetSum){
-            consider = isPossible(nums,currentIndex + 1,targetSum - nums[currentIndex],map);
+        if(nums[currentIndex] <= target){
+            consider=isPartition(nums,currentIndex + 1,target-nums[currentIndex],map);
         }
         if(consider){
             map.put(currentKey,true);
             return true;
         }
-        boolean not_consider= isPossible(nums,currentIndex + 1,targetSum,map);
-        map.put(currentKey,consider || not_consider);
+        boolean notconsider=isPartition(nums,currentIndex + 1,target,map);
+        map.put(currentKey,consider || notconsider);
         return map.get(currentKey);
     }
 }
