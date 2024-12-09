e24436bd334a8a2ff2cfa36c073478fab6129b20
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-12-09 22:08:34 +0530

    Time: 7 ms (41.50%) | Memory: 68.8 MB (27.36%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,36 +10,47 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        if(head==null || head.next == null)return true;
-
-        ListNode slow=head;
-        ListNode fast=head;
-        while(fast.next != null && fast.next.next != null){
-            slow=slow.next;
-            fast=fast.next.next;
-        }
-        slow.next=reverse(slow.next);
-        slow=slow.next;
-
-        while(slow!= null){
-            if(head.val != slow.val){
-                return false;
-            }
-               
-                head=head.next;
-                 slow=slow.next;
+        if(head==null || head.next==null)return true;
+        ListNode midNode=middleNode(head);
+        ListNode righthalf=midNode.next;
+        midNode.next=null;
+        ListNode lefthalf=head;
+        return compare(lefthalf,reverseLL(righthalf));
+    }
+    private boolean compare(ListNode leftHalf,ListNode rightHalf){
+        ListNode l1=leftHalf;
+        ListNode l2=rightHalf;
+        while(l1 != null && l2 != null){
+            if(l1.val != l2.val)return false;
+            l1=l1.next;
+            l2=l2.next;
         }
         return true;
     }
-    ListNode reverse(ListNode head){
-        ListNode pre=null;
-        ListNode next=null;
-        while(head != null){
-            next=head.next;
-            head.next=pre;
-            pre=head;
-            head=next;
+    private ListNode middleNode(ListNode head) {
+        if(head==null)return null; 
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast.next != null && fast.next.next != null){
+            
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        return pre;
+        // if(fast.next != null){
+        //     return slow.next;
+        // }
+        return slow;
+    }
+    private ListNode reverseLL(ListNode head){
+        ListNode prev=null;
+        ListNode temp=null;
+        ListNode curr=head;
+        while(curr != null){
+            temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
     }
 }
