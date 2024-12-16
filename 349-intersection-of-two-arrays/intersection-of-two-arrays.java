fa7242157a052648a83fc2ed510970b610f7afe9
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-12-16 23:09:45 +0530

    Time: 4 ms (34.11%) | Memory: 43.2 MB (56.71%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,19 +1,27 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        HashSet<Integer> set= new HashSet<>();
-        for(int num:nums1){
-            set.add(num);
-        }
-        HashSet<Integer> intersection=new HashSet<>();
-        for(int num:nums2){
-            if(set.contains(num)){
-                intersection.add(num);
+        ArrayList<Integer> list= new ArrayList<>();
+        for(int i=0;i<nums1.length;i++){
+            boolean isAlreadyAdded=false;
+            for(int k=0;k<list.size();k++){
+                if(list.get(k) == nums1[i]){
+                    isAlreadyAdded=true;
+                    break;
+                }
+            }
+            if(isAlreadyAdded){
+                continue;
+            }
+            for(int j=0;j<nums2.length;j++){
+                if(nums1[i]==nums2[j]){
+                    list.add(nums1[i]);
+                    break;
+                }
             }
         }
-        int[] res= new int[intersection.size()];
-        int index=0;
-        for(int num:intersection){
-            res[index++]=num;
+        int[] res=new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            res[i]=list.get(i);
         }
         return res;
     }
