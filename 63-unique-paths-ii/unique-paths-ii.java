3e646eb2a0f369d548a49ea6dc5b0638e92e30fa
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-12-22 19:07:20 +0530

    Time: 8 ms (7.08%) | Memory: 43.3 MB (7.96%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,20 +1,18 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        return maxPath(0,0,obstacleGrid.length,obstacleGrid[0].length,
-        obstacleGrid,new HashMap<String,Integer>());
+        return possibleWays(obstacleGrid,obstacleGrid.length,obstacleGrid[0].length,0,0,new HashMap<String,Integer>());
     }
-    private int maxPath(int row,int col,int m,int n,int[][] grid,HashMap<String,Integer>map){
-       if(row >= m || col >= n || grid[row][col] == 1)return 0;
-       if(row == m-1 && col==n-1 && grid[row][col] != 1)return 1;
-
-        String currentKey=Integer.toString(row) + "-"+Integer.toString(col);
+     private int possibleWays(int[][] grid,int m,int n,int row,int col,HashMap<String,Integer> map){
+         if(row >= m || col >= n || grid[row][col] == 1)return 0;
+        if(row == m-1 && col == n-1 && grid[row][col] != -1)return 1;
+       
+        String currentKey=Integer.toString(row)+"-"+Integer.toString(col);
         if(map.containsKey(currentKey)){
             return map.get(currentKey);
         }
-       int rightways=maxPath(row,col+1,m,n,grid,map);
-       int downways=maxPath(row + 1,col,m,n,grid,map);
-
-        map.put(currentKey,(rightways + downways));
-       return map.get(currentKey);
+        int right=possibleWays(grid,m,n,row,col+1,map);
+        int down=possibleWays(grid,m,n,row+1,col,map);
+        map.put(currentKey,(right + down));
+        return map.get(currentKey);
     }
 }
