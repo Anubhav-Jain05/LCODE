641cf5dc16a43a609e31c9bd9a36e04e0e79fa6d
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-12-22 17:22:46 +0530

    Time: 21 ms (6.10%) | Memory: 60 MB (5.10%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,23 +1,24 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        return maxProfit(prices,0,1,new HashMap<String,Integer>());
+         return maxProfits(prices,0,1,new HashMap<String,Integer>());
     }
-    private int maxProfit(int[] prices,int currentDay,int canBuy,HashMap<String,Integer>map){
-        if(currentDay >= prices.length)return 0;
-        String currentKey=currentDay +"-" + canBuy;
+    private int maxProfits(int[] prices,int currentIndex,int canBuy,HashMap<String,Integer>map){
+        if(currentIndex >= prices.length)return 0;
+        String currentKey=currentIndex +"-"+canBuy;
+
         if(map.containsKey(currentKey)){
-           return  map.get(currentKey);
+            return map.get(currentKey);
         }
-        int ans =0;
-        if(canBuy == 1){
-            int ideal = maxProfit(prices,currentDay + 1,canBuy,map);
-            int buy= - prices[currentDay] + maxProfit(prices,currentDay  + 1,0,map);
-            ans = Math.max(ideal,buy);
+        int ans=0;
+        if(canBuy==1){
+            int ideal=maxProfits(prices,currentIndex + 1,canBuy,map);
+            int buy=-prices[currentIndex] + maxProfits(prices,currentIndex + 1,0,map);
+            ans=Math.max(ideal,buy);
         }
-        else {
-            int ideal=maxProfit(prices,currentDay +1,canBuy,map);
-            int sell= prices[currentDay] + maxProfit(prices,currentDay +1, 1,map);
-            ans = Math.max(ideal,sell);
+        else{
+            int ideal=maxProfits(prices,currentIndex + 1,canBuy,map);
+            int sell=prices[currentIndex] + maxProfits(prices,currentIndex + 1,1,map);
+            ans=Math.max(ideal,sell);
         }
         map.put(currentKey,ans);
         return ans;
