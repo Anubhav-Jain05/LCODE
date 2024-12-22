9d11b576eaa088aa9b7fc4074ff2d30a33e37ccb
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-12-22 14:55:39 +0530

    Time: 1203 ms (5.20%) | Memory: 61.7 MB (5.30%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,27 +1,23 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int ans= minimumWays(coins,0,amount,new HashMap<String,Integer>());
-
-        if(ans == 10001)return -1;
-
+  public int coinChange(int[] coins,int amount) {
+        int ans= totalWays(amount,0,coins,new HashMap<String,Integer>());
+        if(ans== 10001)return -1;
         return ans;
     }
-   private int minimumWays(int[] coins, int currentIndex, int amt, HashMap<String, Integer> map) {
-        if (amt == 0)
-            return 0;
-        if (currentIndex == coins.length)
-            return 10001;
-        String currentKey = Integer.toString(currentIndex) + "-" + Integer.toString(amt);
-        if (map.containsKey(currentKey)) {
+    private int totalWays(int amt,int currentIndex,int[] coins,HashMap<String,Integer>map){
+        if(amt == 0)return 0;
+        if(currentIndex == coins.length)return 10001;
+        String currentKey=Integer.toString(currentIndex) + "-" + Integer.toString(amt);
+        if(map.containsKey(currentKey)){
             return map.get(currentKey);
         }
-        int currentCoin = coins[currentIndex];
-        int consider = 10001;
-        if (currentCoin <= amt) {
-            consider = 1 + minimumWays(coins, currentIndex, amt - currentCoin, map);
+        int current_coin=coins[currentIndex];
+        int consider=10001;
+        if(current_coin <= amt){
+            consider=1 + totalWays(amt-current_coin,currentIndex,coins,map);
         }
-        int not_consider = minimumWays(coins, currentIndex + 1, amt, map);
-        map.put(currentKey, Math.min(consider ,not_consider));
-        return map.get(currentKey);
+        int notConsider=totalWays(amt,currentIndex + 1,coins,map);
+        map.put(currentKey,Math.min(consider,notConsider));
+        return  map.get(currentKey);
     }
 }
