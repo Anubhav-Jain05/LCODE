ae1a66a8ca8545d39e6ad1c8038fd1ab76ca13e6
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-12-22 17:19:26 +0530

    Time: 123 ms (5.79%) | Memory: 79.9 MB (5.09%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,21 +1,23 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
-        return maxProfit(prices,0,1,k,new HashMap<String,Integer>());
+         return maxProfits(prices,0,1,k,new HashMap<String,Integer>());
     }
-    private int maxProfit(int[] prices,int currentDay,int canBuy,int transcount,HashMap<String,Integer>map){
-        if(currentDay >= prices.length || transcount == 0)return 0;
-        String currentKey=currentDay +"-"+canBuy+"-"+transcount;
+    private int maxProfits(int[] prices,int currentIndex,int canBuy,int transCount,HashMap<String,Integer>map){
+        if(currentIndex >= prices.length || transCount == 0)return 0;
+        String currentKey=currentIndex +"-"+canBuy+"-"+transCount;
+
         if(map.containsKey(currentKey)){
             return map.get(currentKey);
         }
         int ans=0;
-        if(canBuy == 1){
-            int ideal= maxProfit(prices,currentDay + 1,canBuy,transcount,map);
-            int buy= - prices[currentDay] + maxProfit(prices,currentDay +1 ,0,transcount,map);
+        if(canBuy==1){
+            int ideal=maxProfits(prices,currentIndex + 1,canBuy,transCount,map);
+            int buy=-prices[currentIndex] + maxProfits(prices,currentIndex + 1,0,transCount,map);
             ans=Math.max(ideal,buy);
-        }else{
-            int ideal = maxProfit(prices,currentDay +1,canBuy,transcount,map);
-            int sell=prices[currentDay] + maxProfit(prices,currentDay +1 ,1,transcount-1,map);
+        }
+        else{
+            int ideal=maxProfits(prices,currentIndex + 1,canBuy,transCount,map);
+            int sell=prices[currentIndex] + maxProfits(prices,currentIndex + 1,1,transCount-1,map);
             ans=Math.max(ideal,sell);
         }
         map.put(currentKey,ans);
