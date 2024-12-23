5e2e2acd2fafd49bfa24a35538673d96b6d42815
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2024-12-24 03:27:36 +0530

    Time: 64 ms (8.63%) | Memory: 45.6 MB (23.64%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -1,28 +1,34 @@
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
         List<List<Integer>> list= new ArrayList<>();
-        HashSet<Integer> set = new HashSet<>();
-        HashSet<Integer> set1=new HashSet<>();
+        HashMap<Integer,Boolean> map= new HashMap<>();
         for(int num:nums1){
-            set.add(num);
-        }
-        for(int num : nums2){
-            set1.add(num);
-        }
-        ArrayList<Integer> res= new ArrayList<>();
-        for(int num:nums1){
-            if(!set1.contains(num) && !res.contains(num)){
-                res.add(num);    
+            if(!map.containsKey(num)){
+                map.put(num,true);
             }
         }
-        ArrayList<Integer> res1=new ArrayList<>();
-        for(int num : nums2){
-            if(!set.contains(num) && !res1.contains(num)){
-                res1.add(num);   
+        HashMap<Integer,Boolean> map2= new HashMap<>();
+        ArrayList<Integer> res= new ArrayList<>();
+        for(int num:nums2){
+            if(!map2.containsKey(num)){
+                // list.add(num);
+                map2.put(num,true);
+
+            }
+        }
+        ArrayList<Integer> res2=new ArrayList<>();
+        for(int num : nums1){
+            if(!map2.containsKey(num) && !res.contains(num)){
+                res.add(num);
+            }
+        }
+         for(int num : nums2){
+            if(!map.containsKey(num) && !res2.contains(num)){
+                res2.add(num);
             }
         }
         list.add(res);
-        list.add(res1);
-    return list;
+        list.add(res2);
+        return list;
     }
 }
