44c41bc66a8ddb1aca420c6944232c3192bd61bb
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2025-01-01 12:57:57 +0530

    Time: 1 ms (99.81%) | Memory: 61.1 MB (94.05%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,26 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        return maxProfits(prices,0,1,1,new HashMap<String,Integer>());
-    }
-    private int maxProfits(int[] prices,int currentIndex,int canBuy,int transCount,HashMap<String,Integer>map){
-        if(currentIndex >= prices.length || transCount == 0)return 0;
-        String currentKey=currentIndex +"-"+canBuy+"-"+transCount;
-
-        if(map.containsKey(currentKey)){
-            return map.get(currentKey);
+        int min=Integer.MAX_VALUE;
+        int max=0;
+        for(int i=0;i<prices.length;i++){
+            min=Math.min(min,prices[i]);
+            max=Math.max(max,prices[i]-min);
         }
-        int ans=0;
-        if(canBuy==1){
-            int ideal=maxProfits(prices,currentIndex + 1,canBuy,transCount,map);
-            int buy=-prices[currentIndex] + maxProfits(prices,currentIndex + 1,0,transCount,map);
-            ans=Math.max(ideal,buy);
-        }
-        else{
-            int ideal=maxProfits(prices,currentIndex + 1,canBuy,transCount,map);
-            int sell=prices[currentIndex] + maxProfits(prices,currentIndex + 1,1,transCount-1,map);
-            ans=Math.max(ideal,sell);
-        }
-        map.put(currentKey,ans);
-        return ans;
+        return max;
     }
 }
