bf1d18e2f9bb3d86fe2a136f30cb1218f72833d5
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2025-01-22 03:16:11 +0530

    Time: 2 ms (88.87%) | Memory: 55.2 MB (85.70%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -4,12 +4,12 @@ class Solution {
         int[] ans=new int[nums.length];
         ans[0]=1;
         for(int i=1;i<n;i++){
-            ans[i] =ans[i-1] * nums[i-1];
+            ans[i]=ans[i-1] * nums[i-1];
         }
         int mul=1;
-        for(int i = n-1;i>=0;i--){
-            ans[i] *=mul;
-            mul *= nums[i];
+        for(int i=n-1;i>=0;i--){
+            ans[i]=ans[i] * mul;
+            mul =mul * nums[i];
         }
         return ans;
     }
