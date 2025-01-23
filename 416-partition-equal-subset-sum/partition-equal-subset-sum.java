e79f1475207ceddefa07cf60f1de425caa20bbc3
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2025-01-23 07:13:37 +0530

    Time: 607 ms (5.01%) | Memory: 201.1 MB (5.02%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,31 +1,30 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        HashMap<String,Boolean>map= new HashMap<>();
+        HashMap<String,Boolean>map=new HashMap<>();
         int target=0;
         for(int num:nums){
             target += num;
         }
-        if(target % 2 != 0)return false;
-        return isPartition(nums,0,target / 2,map);
+        if(target %2 != 0)return false;
+        return isSum(nums,0,target / 2,map);
     }
-    private boolean isPartition(int[] nums,int currentIndex,int target,HashMap<String,Boolean>map){
+    private boolean isSum(int[] nums,int currentIndex,int target,HashMap<String,Boolean> map){
         if(target == 0)return true;
         if(currentIndex >= nums.length)return false;
-
         String currentKey=Integer.toString(currentIndex) +"-"+Integer.toString(target);
         if(map.containsKey(currentKey)){
             return map.get(currentKey);
         }
         boolean consider=false;
         if(nums[currentIndex] <= target){
-            consider= isPartition(nums,currentIndex + 1,target-nums[currentIndex],map);
+            consider=isSum(nums,currentIndex + 1,target-nums[currentIndex],map);
         }
         if(consider){
             map.put(currentKey,true);
             return true;
         }
-        boolean notConsider=isPartition(nums,currentIndex + 1,target,map);
-        map.put(currentKey,consider || notConsider);
+        boolean not_consider=isSum(nums,currentIndex + 1,target,map);
+        map.put(currentKey,consider || not_consider);
         return map.get(currentKey);
     }
 }
