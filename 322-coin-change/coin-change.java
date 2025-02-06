fe35139c2f406f71135b257a4ad8c31d48a97b00
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2025-02-07 00:01:36 +0530

    Time: 826 ms (5.14%) | Memory: 59.2 MB (5.01%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,23 +1,23 @@
 class Solution {
-  public int coinChange(int[] coins,int amount) {
-        int ans= totalWays(amount,0,coins,new HashMap<String,Integer>());
-        if(ans== 10001)return -1;
+    public int coinChange(int[] coins, int amount) {
+        int ans=minCoin(coins,amount,0,new HashMap<String,Integer>());
+        if(ans == 100001)return -1;
         return ans;
     }
-    private int totalWays(int amt,int currentIndex,int[] coins,HashMap<String,Integer>map){
+    private int minCoin(int[] coins,int amt,int currentIndex,HashMap<String,Integer>map){
         if(amt == 0)return 0;
-        if(currentIndex == coins.length)return 10001;
-        String currentKey=Integer.toString(currentIndex) + "-" + Integer.toString(amt);
+        if(currentIndex == coins.length)return 100001;
+        String currentKey=currentIndex +"-"+amt;
         if(map.containsKey(currentKey)){
             return map.get(currentKey);
         }
-        int current_coin=coins[currentIndex];
-        int consider=10001;
-        if(current_coin <= amt){
-            consider=1 + totalWays(amt-current_coin,currentIndex,coins,map);
+        int curr_coins=coins[currentIndex];
+        int consider=100001;
+        if(curr_coins <= amt){
+            consider = 1 + minCoin(coins,amt - curr_coins,currentIndex,map);
         }
-        int notConsider=totalWays(amt,currentIndex + 1,coins,map);
-        map.put(currentKey,Math.min(consider,notConsider));
-        return  map.get(currentKey);
+        int notconsider=minCoin(coins,amt,currentIndex + 1,map);
+        map.put(currentKey,Math.min(consider,notconsider));
+        return map.get(currentKey);
     }
 }
