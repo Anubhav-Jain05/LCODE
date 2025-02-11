dde9e07ac31851536900c1186350b8449bc6eb00
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2025-02-11 23:31:56 +0530

    Time: 3 ms (63.07%) | Memory: 53.9 MB (61.96%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -2,14 +2,14 @@ class Solution {
     public int findJudge(int n, int[][] trust) {
         int[] indegree=new int[n+1];
         int[] outdegree=new int[n+1];
-        for(int[] tru:trust){
+        for(int[] tru:trust ){
             int a=tru[0];
             int b=tru[1];
             indegree[b] += 1;
             outdegree[a] += 1;
         }
         for(int i=1;i<=n;i++){
-            if(indegree[i] == n-1 && outdegree[i] == 0){
+            if(indegree[i]==n-1 && outdegree[i]==0){
                 return i;
             }
         }
