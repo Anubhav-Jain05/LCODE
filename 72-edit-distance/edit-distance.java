31c5b38a8bbc54b245333b00122bf8593923c124
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2025-03-02 23:55:52 +0530

    Time: 5 ms (64.42%) | Memory: 45.1 MB (61.81%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,25 +1,20 @@
 class Solution {
-    public int minDistance(String s1, String s2) {
-        return minimum_op(s1,s2,0,0,s1.length(),s2.length(),new HashMap<String,Integer>());
-    }
-    private int minimum_op(String s1,String s2,int i ,int j,int n,int m,HashMap<String,Integer>map){
-        if(j >= m)return n-i;
-        if(i >= n)return m-j;
-        int ans=1000000;
-        String currentKey=i +"-" + j;
-        if(map.containsKey(currentKey)){
-            return map.get(currentKey);
-        }
-        if(s1.charAt(i)== s2.charAt(j)){
-            ans=minimum_op(s1,s2,i+1,j+1,n,m,map);
-        }else{
-            int insertion= 1 + minimum_op(s1,s2,i,j+1,n,m,map);
-            int deletion= 1 + minimum_op(s1,s2,i+1,j,n,m,map);
-            int replace= 1 + minimum_op(s1,s2,i+1,j+1,n,m,map);
-            ans=Math.min(insertion,Math.min(deletion,replace));
-        }
-        map.put(currentKey,ans);
-        return ans;
-    }
+    public int minDistance(String word1, String word2) {
+        int n=word1.length();
+        int m=word2.length();
+        int[][]dp=new int[n+1][m+1];
+        for(int i=0;i<=n;i++)dp[i][0]=i;
+        for(int j=0;j<=m;j++)dp[0][j]=j;
 
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }else{
+                    dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i][j-1],dp[i-1][j]));
+                }
+            }
+        }
+        return dp[n][m];
+    }
 }
