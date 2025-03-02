46c9c8f18f0d4dde87c7927cc0a450136b512c7d
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2025-03-02 17:38:23 +0530

    Time: 56 ms (40.63%) | Memory: 55.3 MB (42.25%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -5,7 +5,7 @@ class Solution {
     private int maxSeq(String text1,String text2){
         int n=text1.length();
         int m=text2.length();
-        int dp[][]=new int[n+1][m+1];
+        int[][] dp =new int[n+1][m+1];
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(text1.charAt(i-1) == text2.charAt(j-1)){
